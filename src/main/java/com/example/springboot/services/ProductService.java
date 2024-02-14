fd13891f6b48336052adba70448d3dc31f379675
Luquebas <lucas.fonseca.sesisenaisp@gmail.com>
2024-02-14 17:33:47 -0300

    Create Product Service

---
+++ src/main/java/com/example/springboot/services/ProductService.java
@@ -0,0 +1,50 @@
+package com.example.springboot.services;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.springboot.models.ProductModel;
+import com.example.springboot.repositories.ProductRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class ProductService {
+    
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional
+    public ProductModel create(ProductModel productModel) {
+        productModel.setIdProduct(null);
+        return productRepository.save(productModel);
+    }
+
+    public ProductModel findById(UUID id) {
+        Optional<ProductModel> product = productRepository.findById(id);
+        return product.orElseThrow(() -> new RuntimeException("Not Found"));
+    }
+
+    public List<ProductModel> findAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Transactional
+    public ProductModel update(ProductModel productModel) {
+        ProductModel newProduct = findById(productModel.getIdProduct());
+        return productRepository.save(newProduct);
+    }
+
+    @Transactional
+    public void delete(UUID id) {
+        findById(id);
+        try {
+            productRepository.deleteById(id);
+        } catch(Exception e) {
+            throw new RuntimeException("Can't Delete");
+        }
+    }
+
+
+}
